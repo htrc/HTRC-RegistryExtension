2f08100b30c25ee8d1c45d9564a27d029cf2dd60
Boris Capitanu <borice@hotmail.com>
2015-02-22 21:42:11 -0600

    - added public (unauthenticated) API for retrieving public worksets

---
+++ ExtensionAPI/src/main/java/edu/illinois/i3/htrc/registry/api/workset/PublicVolumesAPI.java
@@ -0,0 +1,55 @@
+package edu.illinois.i3.htrc.registry.api.workset;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import edu.illinois.i3.htrc.registry.api.HTRCMediaTypes;
+
+/**
+ * JAX-RS API Interface for accessing workset volumes of public worksets
+ *
+ * @author capitanu
+ *
+ */
+
+@Produces({
+	HTRCMediaTypes.VOLUME_XML,
+	HTRCMediaTypes.VOLUME_JSON,
+	MediaType.APPLICATION_XML,
+	MediaType.APPLICATION_JSON
+})
+public interface PublicVolumesAPI {
+
+	/**
+	 * GET: Retrieve workset volumes as plain-text
+	 *
+	 * @param author The workset author
+	 * @return The workset volumes
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getVolumesAsPlainText(@QueryParam("author") String author);
+
+	/**
+	 * GET: Retrieve workset volumes as CSV
+	 *
+	 * @param author The workset author
+	 * @return The workset volumes
+	 */
+	@GET
+	@Produces(HTRCMediaTypes.TEXT_CSV)
+	public Response getVolumesAsCSV(@QueryParam("author") String author);
+
+	/**
+	 * GET: Retrieve the workset volumes as XML or JSON
+	 *
+	 * @param author The workset author
+	 * @return
+	 */
+	@GET
+	public Response getVolumes(@QueryParam("author") String author);
+
+}
---
+++ ExtensionAPI/src/main/java/edu/illinois/i3/htrc/registry/api/workset/PublicWorksetsAPI.java
@@ -0,0 +1,45 @@
+package edu.illinois.i3.htrc.registry.api.workset;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import edu.illinois.i3.htrc.registry.api.HTRCMediaTypes;
+
+/**
+ * JAX-RS Interface for accessing public Worksets
+ *
+ * @author capitanu
+ *
+ */
+
+@Path("/")
+@Produces({
+	HTRCMediaTypes.WORKSET_XML,
+	HTRCMediaTypes.WORKSET_JSON,
+	MediaType.APPLICATION_XML,
+	MediaType.APPLICATION_JSON
+})
+public interface PublicWorksetsAPI {
+
+	/**
+	 * GET: Retrieve list of public worksets
+	 *
+	 * @return The list of public worksets
+	 */
+	@GET
+	public Response getPublicWorksets();
+
+	/**
+	 * Get access to a {@link PublicWorksetAPI} instance used to manage individual public worksets
+	 *
+	 * @param id The workset id (name)
+	 * @return The {@link PublicWorksetAPI}
+	 */
+	@Path("/{id}")
+	public PublicWorksetAPI getPublicWorksetAPI(@PathParam("id") String id);
+
+}
